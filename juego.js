a8627434b123359b84641be4047119867846982c
Fabricio <fabri_13_03@hotmail.com>
2021-02-06 22:44:04 -0300

    otro for

--- juego.js
+++ juego.js
@@ -2,4 +2,14 @@ const prueba = 10;
 
 const arreglo=[1,2,3,4,5,6];
 
-arreglo.forEach(numero => console.log(numero)); 
+arreglo.forEach(numero => console.log(numero));
+
+const nuevo="hola";
+
+let texto = "hola gente"
+
+for(let i =0; i < texto.length ;i++){
+    
+    console.log("contando letras " + texto[i].split(" "));
+}
+
